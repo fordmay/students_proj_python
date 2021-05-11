af5c674acc6ed7a4ef2237f1884137dcaa4e24d9
dmytro <d.u.zheliabin@gmail.com>
2021-05-11 21:50:52 +0300

    add model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    """Student Model"""
+    first_name = models.CharField(
+        max_length=256, blank=False, verbose_name="Імʼя"
+    )
+    last_name = models.CharField(
+        max_length=256, blank=False, verbose_name="Прізвище"
+    )
+    middle_name = models.CharField(
+        max_length=256, blank=True, verbose_name="По-батькові", default=''
+    )
+    birthday = models.DateField(
+        blank=False, verbose_name="Дата народження", null=True
+    )
+    photo = models.ImageField(
+        blank=True, verbose_name="Фото", null=True
+    )
+    ticket = models.CharField(
+        max_length=256, blank=False, verbose_name="Білет"
+    )
+    notes = models.TextField(
+        blank=True, verbose_name="Додаткові нотатки"
+    )
+
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенти"
+
+    def __str__(self):
+        """Return first_name last_name"""
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.strip()
