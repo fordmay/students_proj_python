1a5b4b094b574eb2baf9eaaee95b3d094102d884
dmytro <d.u.zheliabin@gmail.com>
2021-06-01 22:08:18 +0300

    update groups and students

--- students/views/groups_view.py
+++ students/views/groups_view.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 from students.models import Group
@@ -5,6 +6,16 @@ from students.models import Group
 
 def groups_list(request):
     groups = Group.objects.all()
+
+    order_by = request.GET.get('order_by')
+    if order_by in ('id', 'title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse') == '1':
+            groups = groups.reverse()
+
+    paginator = Paginator(groups, 3)
+    page_number = request.GET.get('page')
+    groups = paginator.get_page(page_number)
     return render(request, 'students/groups_list.html', {'groups': groups})
 
 
--- students/views/students_view.py
+++ students/views/students_view.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 from students.models import Student
@@ -8,24 +8,16 @@ def students_list(request):
     students = Student.objects.all()
 
     # try to order student list
-    order_by = request.GET.get('order_by', '')
+    order_by = request.GET.get('order_by')
     if order_by in ('id', 'last_name', 'first_name', 'ticket'):
         students = students.order_by(order_by)
-        if request.GET.get('reverse', '') == '1':
+        if request.GET.get('reverse') == '1':
             students = students.reverse()
 
     # paginator students
     paginator = Paginator(students, 3)
-    page = request.GET.get('page')
-    try:
-        students = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        students = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        students = paginator.page(paginator.num_pages)
-
+    page_number = request.GET.get('page')
+    students = paginator.get_page(page_number)
     return render(request, 'students/students_list.html', {'students': students})
 
 
